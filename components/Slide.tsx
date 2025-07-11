eeb085409d0abf66a237f4ebf922f6305e7ac38b
Asus <riyajaswani1991@gmail.com>
2025-07-11 23:32:00 +0530

    blackof assignment

---
+++ components/Slide.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+interface SlideProps {
+  title: string;
+  subtitle: string;
+  description: string;
+  image: string;
+}
+
+export default function Slide({ title, subtitle, description, image }: SlideProps) {
+  return (
+    <div className="w-full h-screen flex flex-col p-6">
+      <header className="text-3xl mb-6 mt-6 text-center">Evolving the drive with <span className="font-bold">360-degree</span><br/> nonwoven solutions</header>
+
+      <div className="flex flex-1 items-center justify-between">
+        <div className="w-1/2 ml-32">
+          <h2 className="text-2xl font-semibold mb-4">{subtitle}</h2>
+          <p className="text-lg text-gray-300">{description}</p>
+        </div>
+        <div className="w-1/2 flex justify-center">
+          <Image src={image} alt="slide image" width={800} height={600}/>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ data/slides.ts
@@ -0,0 +1,44 @@
+export const slides = [
+  {
+    id: 1,
+    title: "Welcome to Our Site",
+    subtitle: "Commercial vehicles",
+    description: "Advancing Nonwoven engineering for heavy-duty vehicles.",
+    image: "/Cabin.png", // Place your image in public folder
+  },
+  {
+    id: 2,
+    title: "Super Fast",
+    subtitle: "Passenger vehicles",
+    description: "Revving up Nonwoven innovation from interior to exterior.",
+    image: "/Commercial 3.png",
+  },
+  {
+    id: 3,
+    title: "Mobile Friendly",
+    subtitle: "Commercial vehicles",
+    description: "Advancing Nonwoven engineering for heavy-duty vehicles.",
+    image: "/Exterior.png",
+  },
+  {
+    id: 4,
+    title: "Passenger vehicles",
+    subtitle: "Passenger vehicles",
+    description: "Revving up Nonwoven innovation from interior to exterior.",
+    image: "/Front.png",
+  },
+  {
+    id: 5,
+    title: "Mobile Friendly",
+    subtitle: "Commercial vehicles",
+    description: "Advancing Nonwoven engineering for heavy-duty vehicles.",
+    image: "/Trunk.png",
+  },
+  {
+    id: 6,
+    title: "Passenger vehicles",
+    subtitle: "Passenger vehicles",
+    description: "Revving up Nonwoven innovation from interior to exterior.",
+    image: "/Car.png",
+  },
+];
